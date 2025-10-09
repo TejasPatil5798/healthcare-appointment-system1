4b9cf74fd9d310f3eee1ac7eb4015ba4bc44f3f4
TejasPatil5798 <tejaspatil5jul@gmail.com>
2025-10-09 15:14:13 +0530

    Updated patient

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,11 @@ async function fetchJSON(url, opts = {}) {
     try { return await res.json(); } catch (e) { return null; }
 }
 function notify(msg) { alert(msg); }
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
