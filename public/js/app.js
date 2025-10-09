60be23dbbc0ffb3f91fbf4308ddd2923fa9b0f0a
TejasPatil5798 <tejaspatil5jul@gmail.com>
2025-10-09 17:53:55 +0530

    Updated new UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,3 @@ async function fetchJSON(url, opts = {}) {
 }
 function notify(msg) { alert(msg); }
 
-
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.getElementById('nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
